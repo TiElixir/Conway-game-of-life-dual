83b6a73ef3ba0242909b8bf6b62bba1302ce872d
TiElixir <bluecluey6407@gmail.com>
2025-09-05 14:57:19 +0530

    basic Functions

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-#Created main.py
+import matplotlib
+import math
+
+grid_size=(9,9)
+
+grid=[
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+#grid Row, Column
+def getValue(x,y):
+    return(grid[y][x])
+
+def getNeighbours(x,y):
+    #grid (y,x)
+    return((grid[y-1][x-1])+(grid[y-1][x])+(grid[y-1][x+1])+(grid[y][x-1])+(grid[y][x+1])+(grid[y+1][x-1])+(grid[y+1][x])+(grid[y+1][x+1]))
+
+
+
+print(getValue(1,2))
+
+print(getNeighbours(1,2))
